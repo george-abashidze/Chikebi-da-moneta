adacb542c04f336a2d68b1b1c82b078abb13de9e
George <giorgi.abashidze95@gmail.com>
2024-07-26 20:38:34 +0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chikebi_da_moneta
+{
+    public class Game
+    {
+        //ჭიქების რაოდენობა
+        public int glasses;
+
+        //ცდების რაოდენობა
+        public int steps;
+
+        //რომელ ჭიქაშია მონეტა (ინდექსი)
+        public int indexOfCoin;
+
+        //ყოველ ცდაზე შეცვალსო თუარა არჩეული ჭიქა ბოლოში
+        public bool alwaysChangeSelectedGlassAtEnd;
+
+        //ყოველ ცდაზე random -ად აირჩიოს ჭიქა თუ არა
+        public bool randomSelection;
+
+        public int[]? randdomSelections;
+
+        //თუ randomSelection false ა მაშინ ყოველ ცდაზე ამ კონკრეტულ ჭიქას აირჩევს
+        public int selectedGlass;
+
+        //ბოლო ჭიქა რომელიც დარჩება არჩეულთან ერთად
+        public int lastGlassIndex;
+
+
+        public int succesCount;
+        public int failCount;
+
+        public Random rnd = new Random();
+
+ 
+        public void Play() {
+
+            succesCount = 0;
+            failCount = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+
+                //random ინდექსი თუ რომელ ჭიქაში იყოს მონეტა 
+                indexOfCoin = rnd.Next(1, glasses + 1);
+
+                //თუ random-ად კომპიუტერმა უნდა აირჩიოს რომელი ჭიქა უნდა
+                if (randomSelection)
+                {
+                    selectedGlass = randdomSelections[i];
+                }
+
+                //თუ თავიდან არჩეული ჭიქაშია მონეტა დავტოვოთ არჩეული ჭიქა და +- 1 ით ახლო ჭიქა
+                if (indexOfCoin == selectedGlass)
+                {
+                    lastGlassIndex = selectedGlass == steps ? steps - 1 : selectedGlass + 1;
+                }
+                //თუარადა არჩეულ ჭიქასთან ერთად უნდა დავტოვოთ ჭიქა რომელშიც მონეტაა
+                else
+                {
+                    lastGlassIndex = indexOfCoin;
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine($" ---------- Cda:{i + 1} ------------------");
+                Console.WriteLine($"Moneta aris me {indexOfCoin}-e chikashi.");
+                Console.WriteLine($"Tavidan Archeuli chikis nomeria: {selectedGlass}");
+                Console.WriteLine($"Darcha 2 cali chika: {selectedGlass}  da {lastGlassIndex}");
+
+                //თუ ბოლოში ვცვლით არჩეულ ჭიქას მეორე დარჩენილზე
+                if (alwaysChangeSelectedGlassAtEnd)
+                {
+                    selectedGlass = lastGlassIndex;
+                    Console.WriteLine($"Shecvala Archeuli chika me {selectedGlass}-e chikit");
+                }
+
+
+
+                if (indexOfCoin == selectedGlass)
+                    succesCount++;
+                else
+                    failCount++;
+
+            }
+
+            Console.WriteLine($"Success Count: {succesCount}");
+            Console.WriteLine($"Fail Count: {failCount}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Chikebi_da_moneta;
+
+Random rnd = new Random();
+bool exit = false;
+var game = new Game();
+
+try
+{
+
+    while (!exit)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("chikebis raodenoba: minimaluri 3");
+
+        game.glasses = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", ""));
+
+        if (game.glasses < 3)
+            game.glasses = 3;
+
+
+        Console.WriteLine("cdebis raodenoba: min 1 max 20");
+        game.steps = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", ""));
+
+        if(game.steps > 20)
+            game.steps = 20;
+
+        game.randdomSelections = new int[game.steps];
+
+        for (int i = 0; i < game.steps; i++)
+        {
+            game.randdomSelections[i] = rnd.Next(1, game.glasses + 1);
+        }
+
+        Console.WriteLine("kovel cdaze shecvalos tuara archeuli chika boloshi: 1 ki 0 ara");
+        game.alwaysChangeSelectedGlassAtEnd = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", "")) == 1;
+
+        Console.WriteLine("yovel cdaze random -ad airchios kompma tu ara romeli chiqa unda: 1 ki 0 ara");
+        game.randomSelection = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", "")) == 1;
+
+        if (!game.randomSelection)
+        {
+            Console.WriteLine("romeli chika airchios yovel cdaze? ricxvi 1 dan " + game.glasses + "-mde");
+            game.selectedGlass = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", ""));
+
+            if (game.selectedGlass > game.glasses)
+            {
+                game.selectedGlass = game.glasses;
+            }
+
+        }
+
+        game.Play();
+
+        Console.WriteLine("");
+        Console.WriteLine("1 Tavidan Cda 0 Gasvla");
+        exit = Convert.ToInt32(Console.ReadLine()?.Trim().Replace(" ", "")) != 1;
+    }
+
+
+}
+catch
+{
+   
+}
